40a1b45d985097a0c76cf1ce8ff6a264d75bc20c
Henoch KOFFI LONZO <hlonzo@quantech.solutions>
2024-04-10 01:04:28 +0000

    Initialize Person Model && Controller

---
+++ src/main/java/com/example/test/model/Person.java
@@ -0,0 +1,12 @@
+package com.example.test.model;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    public Person(String id, String firstName, String lastName, LocalDate dateOfBirth) {}
+
+}
